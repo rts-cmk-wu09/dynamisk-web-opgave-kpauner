0a5f4c7ca4f40b6899ceff64d6e38b71f6f48471
kpauner <kpauner@gmail.com>
2023-02-14 08:29:56 +0100

    add services

--- js/index.js
+++ js/index.js
@@ -1,8 +1,37 @@
 document.addEventListener("DOMContentLoaded", function() {
+    let body = document.querySelector("body");
 
+    let heroSection = document.querySelector('.hero');
+    let servicesSection = document.querySelector('.services');
     // slå dig løs her... 
+    
 
-    //eksempel på at udskrive alle overskrifter i services i konsollen:
-    services.forEach(service => console.log(service.headline))
+    heroSection.innerHTML = `
+    <header><img src="${hero.image}"></header>
+    <div class="herobox">
+        <h1>${hero.headline}</h1>
+        <p>${hero.copy}</p>
+        <button><img src="${hero.icon}">hejsa</button>
+    </div>
+    `;
+
+  
+    services.forEach((service) => {
+        let serviceslist = document.createElement('article');
+        serviceslist.innerHTML += `
+        <img src="${service.illustration}">
+        <h2>${service.headline}</h2>
+        `;
+        serviceslist.classList.add('card');
+
+        servicesSection.append(serviceslist);      
+    });
+ 
+    let footer = document.createElement('footer');
+    body.append(footer);
+    console.log(footer);
+
+    console.log(servicesSection);
+
 
 }) // DOMContentLoaded slut
