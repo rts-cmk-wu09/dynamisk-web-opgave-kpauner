b325af83e1c6ba9c29688e576af8bb766984e8e7
kpauner <kpauner@gmail.com>
2023-02-15 10:50:34 +0100

    add sites

--- js/index.js
+++ js/index.js
@@ -4,29 +4,38 @@ document.addEventListener("DOMContentLoaded", function() {
     let heroSection = document.querySelector('.hero');
     let servicesSection = document.querySelector('.services');
     let facilitiesSection = document.querySelector('.facilities');
+    let sitesSection = document.querySelector('.sites')
 
     // slå dig løs her... 
     
 
     heroSection.innerHTML = `
-    <header><img src="${hero.image}"></header>
     <div class="herobox">
         <h1>${hero.headline}</h1>
         <p>${hero.copy}</p>
-        <button><img src="${hero.icon}">hejsa</button>
+        <button><img src="${hero.icon}">Explore</button>
     </div>
     `;
+
+    let heroImg = document.createElement('img');
+    heroImg.setAttribute('src', hero.image);
+    heroImg.classList.add('hero-img');
+    heroSection.append(heroImg);
   
     services.forEach((service) => {
         let serviceslist = document.createElement('article');
         serviceslist.innerHTML += `
         <img src="${service.illustration}">
         <h2>${service.headline}</h2>
+        <p>${service.text}</p>
+        <a href="#">${service.linktext}</a>
         `;
-        serviceslist.classList.add('card');
-
+        
+        serviceslist.classList.add('card', 'just-center');
         servicesSection.append(serviceslist);      
     });
+
+    // FACILITIES
 
     let facilitiesHeader = document.createElement('h2')
     facilitiesHeader.classList.add('section-header');
@@ -37,15 +46,36 @@ document.addEventListener("DOMContentLoaded", function() {
         let facilitylist = document.createElement('article');
         facilitylist.innerHTML += `
             <img src="${facility.icon}">
-            <h2>${facility.headline}</h2>
+            <h3>${facility.headline}</h3>
             <p>${facility.text}</p>
-            <a href="${facility.link}">Show me more</a>
+            <a href="${facility.link}">Show me more</a>           
 
         `;
+        facilitylist.classList.add('card');
         facilitiesSection.append(facilitylist);
 
     });
     
+    // SITES
+
+    let sitesHeader = document.createElement('div')
+    sitesHeader.innerHTML = `
+        <h2>${sites.headline}</h2>
+        Postnummer: ${sites.headline}
+    `;
+
+    let sitesList = document.createElement('ul')
+    sites.places.forEach((site) => {
+        sitesList.innerHTML += `
+        <li>
+            <img src="${site.img}">
+            <h2>${site.name}</h2>
+            <p>${site.city}</p>
+            <a href="#">View the site</a>
+        </li>`;
+    });
+    sitesSection.append(sitesHeader);
+    sitesSection.append(sitesList);
 
 
     // FOOTER
@@ -54,7 +84,7 @@ document.addEventListener("DOMContentLoaded", function() {
     body.append(footer);
     console.log(footer);
 
-    console.log(servicesSection);
+    console.log(sitesSection);
 
 
 }) // DOMContentLoaded slut
